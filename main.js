07d8c1ec32af12329b094fcb921299d5d92a135e
Alan Chu <alanchu394@gmail.com>
2020-12-04 23:05:33 -0500

    finished

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var grid = new Array(3);
+var gameStatus = 0;
 var currentPlayer = 0;
+var AIPosition=0;
 function node(i,j){
     this.x = i;
     this.y = j;
@@ -8,16 +10,31 @@ function node(i,j){
     //2: O
     this.tag =0;
     this.show = function(){
+        fill(255);
+        rect(this.x*100,this.y*100,99,99);
         if(this.tag == 1){
-            fill(255,255,0);
+            drawCircle(this.x,this.y);
+            
         }else if(this.tag == 2){
-            fill(0,255,255)
+            drawX(this.x,this.y);
         }else{
+
+            
             fill(255);
+
+            
         }
         stroke(0);
-        rect(this.x*100,this.y*100,99,99);
+        
     }
+}
+function drawCircle(i,j){
+    ellipse((i*100)+50,(j*100)+50,75);
+}
+function drawX(i,j){
+    fill(0);
+    textSize(75);
+    text("X",(i*100)+25,(j*100)+75);
 }
 function setup(){
     //create grid
@@ -32,23 +49,58 @@ function setup(){
         }
     }
 
+
 }
 function draw(){
     background(0);
+
     for(var i = 0; i < 3; i++){
         for(var j = 0; j < 3; j++){
             grid[i][j].show();
         }
     }
-    var w = check();
-    if(w != 0){
-        noLoop();
-        console.log(w);
+
+    if(gameStatus == 1){
+        var w = check();
+        
+        if(w != 0){
+            textSize(30);
+            fill(255,0,0);
+            if(w == 1 || w == 2){
+                text("Computer  won!",50,150);
+
+                
+            }
+            
+            if(w == 3){
+                text("Tied!",120,150);
+            }
+            gameStatus = 2;
+            noLoop();
+
+        }
+        if(currentPlayer == 0 && gameStatus == 1){
+            AITurn();
+        }
+        
+        
     }
-    if(currentPlayer == 0){
-        AITurn();
+
+}
+function HFirst(){
+    if (gameStatus == 0){
+        currentPlayer =1;
+        AIPosition = 1;
+        gameStatus =1;
     }
 }
+function AIFirst(){
+    if (gameStatus == 0){
+        currentPlayer =0;
+        gameStatus =1;
+    }
+}
+
 function move(i,j){
     if(currentPlayer == 0){
         grid[i][j].tag = 1;
@@ -76,20 +128,24 @@ function check(){
     for(var i= 0;i<3;i++){
         if(across(grid[i][0],grid[i][1],grid[i][2])){
             winner = grid[i][0].tag;
+            
         }
     }
     //check vertical wins
     for(i= 0;i<3;i++){
         if(across(grid[0][i],grid[1][i],grid[2][i])){
             winner = grid[0][i].tag;
+            
         }
     }
     //check diagonal wins
         if(across(grid[0][0],grid[1][1],grid[2][2])){
             winner = grid[0][0].tag;
+            
         }
         if(across(grid[0][2],grid[1][1],grid[2][0])){
             winner = grid[0][2].tag;
+            
         }
 
     //check for tie
@@ -102,19 +158,22 @@ function check(){
         }
     }
     if(winner == 0 && empty==0){
+        
         winner = 3;
     }
     return winner;
 }
 function mouseClicked(){
-    var clickedX = Math.floor(mouseX/100);
-    var clickedY = Math.floor(mouseY/100);
+    if(gameStatus == 1){
+        var clickedX = Math.floor(mouseX/100);
+        var clickedY = Math.floor(mouseY/100);
 
     if(clickedX >= 0 && clickedX < 3 && clickedY >= 0 && clickedY < 3){
         if(grid[clickedX][clickedY].tag == 0){
             move(clickedX,clickedY);
         }
         
+    }
     }
 }
 function AITurn(){
@@ -156,7 +215,7 @@ function miniMax(board, depth, isMaxing){
             for(var j = 0; j <3; j++){
                 if(board[i][j].tag == 0){
                     board[i][j].tag = 1;
-                    var score = miniMax(board,depth+1,false);
+                    var score = miniMax(board,depth+1,false)-depth;
                     board[i][j].tag = 0;
                     bestScore = max(score,bestScore);
                 }
@@ -169,7 +228,7 @@ function miniMax(board, depth, isMaxing){
             for( j = 0; j <3; j++){
                 if(board[i][j].tag == 0){
                     board[i][j].tag = 2;
-                    score = miniMax(board,depth+1,true);
+                    score = miniMax(board,depth+1,true)+depth;
                     board[i][j].tag = 0;
                     bestScore = min(score,bestScore);
                 }
